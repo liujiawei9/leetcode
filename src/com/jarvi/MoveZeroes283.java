a438a2c0c6e32505965311620bcb074566c7d1cb
Kevin <411190068@qq.com>
2021-03-25 16:38:33 +0800

    update

--- src/com/jarvi/MoveZeroes283.java
+++ src/com/jarvi/MoveZeroes283.java
@@ -43,14 +43,14 @@ public class MoveZeroes283 {
      * @param nums
      */
     public void moveZeroes2(int[] nums) {
-        int j = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0) {
-                if (nums[j] == 0) {
-                    nums[j] = nums[i];
-                    nums[i] = 0;
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                if (nums[i] == 0) {
+                    nums[i] = nums[j];
+                    nums[j] = 0;
                 }
-                j++;
+                i++;
             }
         }
     }
