197a27e987d7e1a31aceb1145de194844b72e614
Kevin <411190068@qq.com>
2021-04-06 11:36:35 +0800

    update

--- src/com/jarvi/Test.java
+++ src/com/jarvi/Test.java
@@ -3,8 +3,7 @@ package com.jarvi;
 public class Test {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i&1);
-        }
+        System.out.println("Hello World!");
     }
+
 }
