c6d2de700bb30cf7e497d52db5d2a89bc6771502
liujiawei9 <411190068@qq.com>
2021-04-14 00:23:40 +0800

    update

--- src/com/jarvi/TwoSum1.java
+++ src/com/jarvi/TwoSum1.java
@@ -34,18 +34,13 @@ import java.util.Map;
 public class TwoSum1 {
 
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
-        int[] result = twoSum(nums, 9);
-        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15}, 9)));
     }
 
     /**
      * HashMap 缓存法
      * 时间复杂度：O(n)
      * 空间复杂度：O(n)
-     * @param nums
-     * @param target
-     * @return
      */
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -56,7 +51,23 @@ public class TwoSum1 {
             }
             map.put(nums[i], i);
         }
-        return null;
+        return new int[0];
     }
 
+    /**
+     * 暴力法,两层循环。
+     * 时间复杂度：O(n^2)
+     * 空间复杂度：O(1)
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
 }
