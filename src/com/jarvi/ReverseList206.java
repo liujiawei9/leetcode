1ab3aae9d4ea0c8aadcf2fdaae8e979dc4c1acaf
Jarvi <411190068@qq.com>
2021-05-09 23:53:06 +0800

    add

---
+++ src/com/jarvi/ReverseList206.java
@@ -0,0 +1,49 @@
+package com.jarvi;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * 206. 反转链表
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * <p>
+ * 示例 2：
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * <p>
+ * 示例 3：
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ */
+public class ReverseList206 {
+
+    public ListNode reverseList(ListNode head) {
+        return head;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
