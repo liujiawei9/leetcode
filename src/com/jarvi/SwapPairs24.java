32f396f20d7f594d68ea2de5d3c17eb6fdaf7eee
Jarvi <411190068@qq.com>
2021-05-11 20:40:36 +0800

    add

---
+++ src/com/jarvi/SwapPairs24.java
@@ -0,0 +1,62 @@
+package com.jarvi;
+
+/**
+ * 24. 两两交换链表中的节点
+ * 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * 示例 1：
+ * 输入：head = [1,2,3,4]
+ * 输出：[2,1,4,3]
+ * <p>
+ * 示例 2：
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 示例 3：
+ * 输入：head = [1]
+ * 输出：[1]
+ * <p>
+ * <p>
+ * 提示：
+ * 链表中节点的数目在范围 [0, 100] 内
+ * 0 <= Node.val <= 100
+ * <p>
+ * 进阶：你能在不修改链表节点值的情况下解决这个问题吗?（也就是说，仅修改节点本身。）
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SwapPairs24 {
+
+
+    /**
+     * 递归法
+     * 时间复杂度：O(n)，其中 n 是链表的节点数量。需要对每个节点进行更新指针的操作。
+     * 空间复杂度：O(n)，其中 n 是链表的节点数量。空间复杂度主要取决于递归调用的栈空间。
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode one = head;
+        ListNode two = head.next;
+        ListNode three = two.next;
+        two.next = one;
+        one.next = swapPairs(three);
+        return two;
+    }
+
+    /**
+     * 迭代法
+     */
+    public ListNode swapPairs2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        //TODO
+        return head;
+    }
+
+}
