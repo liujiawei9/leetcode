9fe974102f90aab2055cf3615d0d774dc19048c8
Jarvi <411190068@qq.com>
2021-05-13 15:52:45 +0800

    update

--- src/com/jarvi/DetectCycle142.java
+++ src/com/jarvi/DetectCycle142.java
@@ -3,13 +3,60 @@ package com.jarvi;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 142. 环形链表 II
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * <p>
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ * 说明：不允许修改给定的链表。
+ * <p>
+ * 进阶：
+ * 你是否可以使用 O(1) 空间解决此题？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class DetectCycle142 {
 
+    /**
+     * 快慢指针法
+     * <p>
+     * 时间复杂度：O(N)，其中 N 为链表中节点的数目。
+     * 在最初判断快慢指针是否相遇时，slow 指针走过的距离不会超过链表的总长度；
+     * 随后寻找入环点时，走过的距离也不会超过链表的总长度。
+     * 因此，总的执行时间为 O(N)+ O(N) = O(N)。
+     * <p>
+     * 空间复杂度：O(1)。我们只使用了三个指针。
+     *
+     * @link https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/142-huan-xing-lian-biao-ii-jian-hua-gong-shi-jia-2/
+     */
     public ListNode detectCycle(ListNode head) {
-       //TODO
+        ListNode slow = head;
+        ListNode fast = head;
+        // 能到达 null，说明不存在环路
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // 快慢指针相遇，说明存在环路
+            if (slow == fast) {
+                //寻找入口
+                fast = head;
+                while (slow != fast) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
         return null;
     }
 
+    /**
+     * hash法
+     * 时间复杂度：O(N)，其中 N 为链表中节点的数目。我们恰好需要访问链表中的每一个节点。
+     * 空间复杂度：O(N)，其中 N 为链表中节点的数目。我们需要将链表中的每个节点都保存在哈希表当中。
+     */
     public ListNode detectCycle2(ListNode head) {
         Set<ListNode> set = new HashSet<>();
         while (head != null) {
